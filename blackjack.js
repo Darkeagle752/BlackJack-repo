cb544565255c4f7a2c5fa8a500706b6eff9ef940
Darkeagle752 <sanchezbd2798@aguafria.org>
2025-03-31 17:44:53 +0000

    message

--- blackjack.js
+++ blackjack.js
@@ -1,4 +1,7 @@
-let dealerCards = 0;
+let dealerCards1 = 0;
+let dealerCards2 = 0;
+dealerCardsTotal = dealerCards1 + dealerCards2;
+
 let playerCards = 0;
 
 function getCards() {
@@ -6,9 +9,13 @@ function getCards() {
 }
 
 function startGame() {
-    dealerCards= getCards() + getCards();
+    dealerCards1 = getCards();
+    dealerCards2 = getCards();
+    dealerCardsTotal = dealerCards1 + dealerCards2;
+
     playerCards = getCards() + getCards();
-    console.log("you have a total of " + playerCards)
+    console.log("you have a total of " + dealerCardsTotal)
+
 }
 
 startGame();
